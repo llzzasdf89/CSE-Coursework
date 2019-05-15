09c97c883594c40137611a3b9919a8261d432098
llzzasdf89 <43372889+llzzasdf89@users.noreply.github.com>
2019-05-15 16:42:45 +0800

    修复了在更新全部书籍数量时无法返回的bug，并且优化了界面提示语

--- CSE104 Assignment2/Library.java
+++ CSE104 Assignment2/Library.java
@@ -13,6 +13,14 @@ public class Library {
 	public boolean isEmpty() {
 		return this.Librarysize==0;
 	}
+	/** when the Library, go back to the userInterface*/
+	public void Empty_back() {
+		if(isEmpty()) {
+			System.out.println("There are no books in the library!\n");
+			System.out.println("========================================");
+			userInterface();
+		}
+	}
 	/** the node in the Linkedlist **/
 	private class Book {//set the node as a inner class, to hide from the user
 		private String bookTitle,bookauthor,category,ISBN;
@@ -32,18 +40,18 @@ public class Library {
 		}
 		@Override
 		public String toString() {
-			return ISBN +"\t/ "+bookTitle+ "\t  / "+bookauthor+"\t/ "+category+"\t/ "+copyNumber+"\t/ " +copyAvailable;
+			return ISBN + " \t/" + bookTitle + " \t/" + bookauthor + " \t/" +  copyNumber + " \t/" + copyAvailable;
 		}
 	}
 	/** Here is the userInterface, index page**/
 	public void userInterface() {
 		System.out.println("Welcome to the library management system, functions provided include the following:\n"
-				+"Add to add a new book\r\n" + 
-				"Update to update book info\r\n" + 
-				"Search to enquire about book info\r\n" + 
-				"Delete to delete a book\r\n" + 
-				"Display to display book(s) info\r\n" + 
-				"Quit to exit the System");
+							+"'Add/add' to add a new book\n" 
+							+"'Update/update' to update book info\n" 
+							+ "'Search/update' to enquire about book info\n"  
+							+ "'Delete/delete' to delete a book\n"  
+							+ "'Display/display' to display book(s) info\n"  
+							+ "'Quit/quit' to exit the System");
 		System.out.println("Enter your command here:");
 		String input  = new Scanner(System.in).nextLine().trim().toLowerCase();
 		switch(input) {
@@ -67,19 +75,19 @@ public class Library {
 	private void addBook() {
 		int copyAvailable=0;
 		String ISBN,author,category,copyNumber,title;
-		System.out.println("Enter a new book ISBN, or input 'quit' to quit");
+		System.out.println("Enter a new book ISBN,the ISBN is 10 or 13 digit number or input 'quit' to quit");
 		ISBN = validateISBN();
-		System.out.println("Enter the title, or input 'quit' to quit");
+		System.out.println("Enter the title : only English letters and spacing allowed, or input 'quit' to quit");
 		title=validateAuthorTitle();
-		System.out.println("Enter the author, or input 'quit' to quit");
+		System.out.println("Enter the author : only English letters and spacing allowed, or input 'quit' to quit");
 		author=validateAuthorTitle();
 		if(validateBook(ISBN,title,author)) userInterface();
-		System.out.println("Please input the category from the list: IT , Arts, Business, Comics, Cooking, Sports, or input 'quit' to quit");
+		System.out.println("Please input the category from the list: [IT , Arts, Business, Comics, Cooking, Sports], or input 'quit' to quit");
 		category= validateCategory();
 		System.out.println("Enter total copy number, or input 'quit' to quit");
 		copyNumber= validateCopyNumber();
 		copyAvailable = Integer.parseInt(copyNumber);
-		System.out.println("Ready to add book: " + ISBN + ";  " + title+ ";  " + author + ";  " + category + ";  " + copyNumber + ";  " + copyAvailable);
+		System.out.println("Ready to add book: " + ISBN + ";  " + title+ ";  " + author + ";  " + category + ";  " + Integer.parseInt(copyNumber) + ";  " + copyAvailable);
 		System.out.println("Enter 'Y/y' to add new book. Anything else to quit");
 		String confirm = new Scanner(System.in).nextLine().trim().toLowerCase();
 		if(!confirm.equals("y")) {
@@ -97,10 +105,7 @@ public class Library {
 	}
 	/** update book **/
 	private void updateBook() {
-		if(isEmpty()) {
-			System.out.println("There are no books in the library!\n");
-			userInterface();
-		}
+		Empty_back();
 		System.out.println("Please input the ISBN or enter 'quit' to quit");
 		String ISBN = validateISBN();
 		String userinput;
@@ -123,56 +128,56 @@ public class Library {
 		userinput = new Scanner(System.in).nextLine();
 		switch(userinput.trim().toUpperCase()) {
 		case "T" : System.out.println("Please input the title you want to change or 'quit' to quit ");
-		String title = validateAuthorTitle();
-		if(validateBook(null,title,cur.bookauthor)) {// pass the current book author and the title user input to checkout whether this book's information has conflict with another book
+		String newTitle = validateAuthorTitle();
+		if(validateBook(null,newTitle,cur.bookauthor)) {// pass the new book author and the title user input to check whether this book's information has conflict with another book in the Library
 			System.out.println("the title you update has been confict with another same book\n");
 			userInterface();
 		};
-		cur.bookTitle = title;
+		cur.bookTitle = newTitle;
 		break;
 		case "A":  System.out.println("Plese input the author you want to change or 'quit' to quit");
-		String author = validateAuthorTitle();
-		if(validateBook(null,cur.bookTitle,author)) {// pass the current book title and the author user input to checkout whether this book's information has conflict with another book
+		String newAuthor = validateAuthorTitle();
+		if(validateBook(null,cur.bookTitle,newAuthor)) {// pass the new book title and the author user input to check whether this book's information has conflict with another book in the Library
 			System.out.println("the author you update has been confict with another same book\n");
 			userInterface();
 		};
-		cur.bookauthor = author;
+		cur.bookauthor = newAuthor;
 		break;
 		case "C":  System.out.println("Please input the category from the list :[IT,Arts, Business, Comics, Cooking, Sports], or enter 'quit' to quit");
-		String category = validateCategory();
-		cur.category = category;
+		String newCategory = validateCategory();
+		cur.category = newCategory;
 		break;
 		case "TC": System.out.println("Please input the total number you want to change,quit to quit");
-		String TotalNumber = validateCopyNumber();
+		String newTotalNumber = validateCopyNumber();
 		while(true) {// prevent the situation in which the new TotalNumber < Available number of book in the laborary
 			try{
-			if(Integer.parseInt(TotalNumber)<cur.copyAvailable) throw new ArithmeticException();
-			else if(Integer.parseInt(TotalNumber)<0) throw new Exception();
-			else if(TotalNumber.trim().toLowerCase().equals("quit")){
+			if(newTotalNumber.trim().toLowerCase().equals("quit")){
 				System.out.println("back to the user Interface\n");
 				userInterface();
 			}
+			else if(Integer.parseInt(newTotalNumber)<0) throw new Exception();
+			else if(Integer.parseInt(newTotalNumber)<cur.copyAvailable) throw new ArithmeticException();
 			else break;
 			}
 			catch(ArithmeticException e) {
 				System.out.println("the copy number your input is less than the copy available, please check and input again, or input 'quit' to go back to user Interface");
-				TotalNumber = new Scanner(System.in).nextLine();
+				newTotalNumber = new Scanner(System.in).nextLine();
 			}
 			catch(Exception e) {
 				System.out.println("input illegal, please input a postive number or input 'quit' to go back to the user Interface" );
-				TotalNumber = new Scanner(System.in).nextLine();
+				newTotalNumber = new Scanner(System.in).nextLine();
 			}
 		}
-		cur.copyNumber = Integer.parseInt(TotalNumber);
+		cur.copyNumber = Integer.parseInt(newTotalNumber);
 		break;
 		case "AC": System.out.println("Please input the available number you want to change,quit to quit");
-		String availableNumber;
+		String newAvailableNumber;
 		while(true){
 			try{
-			availableNumber= new Scanner(System.in).nextLine().trim().toLowerCase();
-			if(availableNumber.toLowerCase().trim().equals("quit")) userInterface();
-			else if(Integer.parseInt(availableNumber)>cur.copyNumber) throw new ArithmeticException();
-			else if(Integer.parseInt(availableNumber)<0) throw new IllegalArgumentException();
+			newAvailableNumber= new Scanner(System.in).nextLine().trim().toLowerCase();
+			if(newAvailableNumber.toLowerCase().trim().equals("quit")) userInterface();
+			else if(Integer.parseInt(newAvailableNumber)>cur.copyNumber) throw new ArithmeticException();
+			else if(Integer.parseInt(newAvailableNumber)<0) throw new IllegalArgumentException();
 			else break;
 			}
 			catch(ArithmeticException e) {
@@ -188,7 +193,7 @@ public class Library {
 				System.out.println("Please input a valuable number again, or input 'quit' to userInterface");
 			}
 		}
-		cur.copyAvailable = Integer.parseInt(availableNumber);
+		cur.copyAvailable = Integer.parseInt(newAvailableNumber);
 		break;
 		default:System.out.println("quit the updating successfully"); userInterface();
 		break;
@@ -198,11 +203,8 @@ public class Library {
 	}
 	/** delete book from library **/
 	private void deleteBook() {
-		if(isEmpty()) {
-			System.out.println("There is no book in the System\n");
-			userInterface();
-		}
-		System.out.println("Enter the book's ISBN or title + author:");
+		Empty_back();
+		System.out.println("Enter the book's ISBN or 'title + author':");
 		String keyword = new Scanner(System.in).nextLine().trim().toLowerCase();
 		Book cur = head;
 		for(int i=0;i<Librarysize;i++) {
@@ -243,10 +245,7 @@ public class Library {
 	}
 	/** search book **/
 	private void searchBook() {
-		if(isEmpty()) {
-			System.out.println("There are no books in the library!\n");
-			userInterface();
-		}
+		Empty_back();
 		System.out.println("Enter 'Y/y' to search books, anything else to quit");
 		String keywords,userinput;
 		userinput = new Scanner(System.in).nextLine().toLowerCase();
@@ -272,17 +271,15 @@ public class Library {
 		}
 	/** display book, traversal the library and print out the books exist in library **/
 	private void displayBook() {
-		if(isEmpty()) {
-			System.out.println("There are no books in the library!\n");
-			userInterface();
-		}
+		Empty_back();
 		Book cur = head;
 		System.out.println("Here are the books in the Library below");
-		System.out.println("ISBN / TITLE / AUTHOR / CATEGORY / TOTAL_COPIES / AVAILABLE_COPIES");
+		System.out.println("ISBN / TITLE/ AUTHOR/ CATEGORY/ TOTAL_COPIES/ AVAILABLE_COPIES");
 		for(int i=0; i<Librarysize; i++) {
-			System.out.println(cur);
+			System.out.println(cur + "\n");
 			cur=cur.Next;
 		}
+		System.out.println("=======================");
 		userInterface();
 	}
 	/** validate book's ISBN,title, author, to make sure there is no repeating book or the same book in library **/
@@ -370,6 +367,4 @@ public class Library {
 		}
 		return copyNumber;
 	}
-	
-	
 }
